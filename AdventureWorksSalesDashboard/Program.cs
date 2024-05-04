6b5c16f90ed7fd2973b5f59cc4d4d3d5a6f10288
David Johansson <david.n.johansson@live.com>
2024-05-04 13:17:56 +0200

    push

--- AdventureWorksSalesDashboard/Program.cs
+++ AdventureWorksSalesDashboard/Program.cs
@@ -1,13 +1,21 @@
 using AdventureWorksSalesDashboard;
 using AdventureWorksSalesDashboard.Entities;
+using AdventureWorksSalesDashboard.Utilities;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<AdventureWorksContext>(options =>
+builder.Services.AddDbContextFactory<AdventureWorksContext>(options =>
 {
-    
+    var dataSource = Secrets.GetValue("DbDataSource");
+    var initialCatalog = Secrets.GetValue("DbInitialCatalog");
+    var userId = Secrets.GetValue("DbUserID");
+    var password = Secrets.GetValue("DbPassword");
+
+    options
+        .UseLazyLoadingProxies()
+        .UseSqlServer($"Data Source={dataSource};Initial Catalog={initialCatalog};User ID={userId};Password={password};Encrypt=False");
 });
 
 builder.Services
